66fdba0eb26cf04f0939e877144f1eade148150c
Daniel Hopkins <d@niestudio.com>
2013-02-26 14:55:10 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studiodb::Application.config.secret_token = 'b1994c03acee1237035f06dd7d3a1bbe52028cff480533d9f0db88bdce2a13f45eb2d584fa0496136103283225f45d685947557e1dd554ac019d538e4f56e586'
