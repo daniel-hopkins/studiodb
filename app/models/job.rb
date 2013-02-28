83255356d8d58974ffab5f126aa6fdb337cfe4ce
Daniel Hopkins <d@niestudio.com>
2013-02-27 23:29:16 -0800

    thumbnails for jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,7 @@
 class Job < ActiveRecord::Base
-  attr_accessible :date, :invoiced, :invoicenumber, :name, :notes, :public, :rawhd, :tifhd, :client_id
+  attr_accessible :date, :invoiced, :invoicenumber, :name, :notes, :public, :rawhd, :tifhd, :client_id, :thumbnail
 
   belongs_to :client
+
+  has_attached_file :thumbnail
 end
