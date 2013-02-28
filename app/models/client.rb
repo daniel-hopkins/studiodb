c19de217db0ece8fa54f609be2359cd4345ac257
Daniel Hopkins <d@niestudio.com>
2013-02-27 18:30:48 -0800

    elvin fixed linkage error

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,5 +2,4 @@ class Client < ActiveRecord::Base
   attr_accessible :name
   
   has_many :jobs
-  belongs_to :job
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,5 @@
 class Job < ActiveRecord::Base
-  attr_accessible :date, :invoiced, :invoicenumber, :name, :notes, :public, :rawhd, :tifhd
+  attr_accessible :date, :invoiced, :invoicenumber, :name, :notes, :public, :rawhd, :tifhd, :client_id
 
   belongs_to :client
-  has_one :client
 end
